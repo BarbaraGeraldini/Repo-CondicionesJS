ff9fe650e5647c7a078c748a23d09cedcb293d0d
BarbaraGeraldini <b.geraldinic@gmail.com>
2024-09-30 00:38:42 -0300

    primer commit

---
+++ stickers/index.js
@@ -0,0 +1,19 @@
+function validarStickers() {
+    const cantidadUno =
+      parseInt(document.querySelector("#cantidadUno").value) || 0;
+    const cantidadDos =
+      parseInt(document.querySelector("#cantidadDos").value) || 0;
+    const cantidadTres =
+      parseInt(document.querySelector("#cantidadTres").value) || 0;
+  
+    const total = cantidadUno + cantidadDos + cantidadTres;
+  
+    const resultado = document.querySelector("#resultado");
+  
+    if (total > 10) {
+      resultado.textContent = "Llevas demasiados stickers";
+    } else {
+      resultado.textContent = `Llevas ${total} stickers`;
+    }
+  }
+  
